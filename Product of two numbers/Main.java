491b4371bbc75efee0ef2c72176262fcbe304af7
Jaipraba <50447303+Jaipraba@users.noreply.github.com>
2019-05-09 16:52:26 +0530

    Test code pushed to repository.

---
+++ Product of two numbers/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+	public static void main (String[] args) {
+		// Type your code here
+      Scanner ni=new Scanner(System.in);
+      int num1=ni.nextInt();
+      int num2=ni.nextInt();
+      int sum=num1*num2;
+      System.out.println(sum);
+	}
+}
